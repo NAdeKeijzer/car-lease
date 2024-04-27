28c9ba279b9a3d6dd95e98e280294e5082d4353b
nikita <n.dk@live.nl>
2024-04-27 12:55:19 +0200

    Customer maintenance implemented

---
+++ src/test/java/com/example/carsaturdayattempt/CarSaturdayAttemptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carsaturdayattempt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSaturdayAttemptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
