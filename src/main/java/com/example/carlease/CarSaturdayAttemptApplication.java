852a7dc49242799ca6ed6df8de773ce5ad16cf21
NAdeKeijzer <36738959+NAdeKeijzer@users.noreply.github.com>
2024-04-28 21:41:48 +0200

    Rename CarLeaseApplication.java

--- src/main/java/com/example/carlease/CarSaturdayAttemptApplication.java
+++ src/main/java/com/example/carlease/CarSaturdayAttemptApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarLeaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CarSaturdayAttemptApplication.class, args);
+        SpringApplication.run(CarLeaseApplication.class, args);
     }
 
 }
